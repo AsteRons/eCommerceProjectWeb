bb7d1e1dcd83a9b16468ce78528440f92d11493a
Maciek Dom <maciej.rudnik2@gmail.com>
2020-08-24 13:15:46 +0200

    Angular Project - Search for Products by Category - Code - Update Spring Boot

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -31,10 +31,12 @@ export class ProductListComponent implements OnInit {
     const hasCategoryId: boolean  = this.route.snapshot.paramMap.has('id');
 
     if(hasCategoryId){
+
     // get the "id" param string, convert string to a number using the "+" symbol
     this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
     }
     else {
+      
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
 
